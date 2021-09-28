aaecfc91da4a24cf78bf0f976a8caed5e96e70a5
Oscar Moreno <omoreno@excentia.es>
2021-09-28 14:59:09 +0200

    Initial commit

---
+++ src/main/java/es/excentia/App.java
@@ -0,0 +1,17 @@
+package es.excentia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        int result = 10 / 0;
+    }
+
+    public static int getLucky() {
+        return 7;
+    }
+}
