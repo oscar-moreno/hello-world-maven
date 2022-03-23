910629f464de032e199022f1ccd8815424831e72
Oscar Moreno <omoreno@excentia.es>
2022-03-23 16:55:10 +0100

    Testing webhook

--- src/main/java/es/excentia/App.java
+++ src/main/java/es/excentia/App.java
@@ -12,7 +12,7 @@ public class App
         String password="My password";
         String string1="My password";
         String string2="My password";
-        String string2="My password";
+        String string3="My password";
     }
 
     public static int getLucky() {
