f31debb62c7b833a34f85dfa976d926a71a8ad00
Oscar Moreno <omoreno@excentia.es>
2022-03-23 17:00:40 +0100

    Solving issues

--- src/main/java/es/excentia/App.java
+++ src/main/java/es/excentia/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args ) {
         String string = "Hello World";
-        System.out.println(string);
     }
 
     public static int getLucky() {
